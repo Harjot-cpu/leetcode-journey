998d7b9627cf5e2e7ac14cd9086ab102a3e0c123
Harjot-cpu <sandhuharjot712@gmail.com>
2026-08-07 00:54:15 +0530

    Time: 2 ms (99.72%) | Memory: 43.7 MB (39.52%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,30 +1,18 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        String ans = "";
-        int count= 0;
-        char[] arr = s.toCharArray();
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i] =='('){
-                if(count ==0){
-                    count +=1;
-                    continue;
-                }
-                else{
-                ans += arr[i];
-                count +=1;
-                }
-            }
-            else{
-                if(count ==0 || count ==1){
-                    count -=1;
-                    continue;
-                }
-                else{
-                    ans += arr[i];
-                    count -=1;
-                }
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (count > 0) ans.append(c);
+                count++;
+            } else {
+                count--;
+                if (count > 0) ans.append(c);
             }
         }
-        return ans;
+
+        return ans.toString();
     }
 }
