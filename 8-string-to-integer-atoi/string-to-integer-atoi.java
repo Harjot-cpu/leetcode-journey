571186c7ae4d27cc2b5a829c37f2afcedaa95c7d
Harjot-cpu <sandhuharjot712@gmail.com>
2026-08-09 10:54:44 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (41.53%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+        while(i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        int sign = 1;
+
+        if(i < n && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+        else if(i < n && s.charAt(i) == '+') {
+            i++;
+        }
+
+        long ans = 0;
+
+        while(i < n && Character.isDigit(s.charAt(i))) {
+
+            ans = ans * 10 + (s.charAt(i) - '0');
+
+       
+            if(sign == 1 && ans > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if(sign == -1 && -ans < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int)(ans * sign);
+    }
+}
