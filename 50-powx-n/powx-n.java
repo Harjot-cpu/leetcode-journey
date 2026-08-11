36ac0330140d855dd7af18ca5212fffedfcddec4
Harjot-cpu <sandhuharjot712@gmail.com>
2026-08-11 13:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (11.75%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        if(n<0){
+            if(n%2==0){
+                       double half = myPow(x,n/2);
+            return half*half;
+            }
+            else{
+                return myPow(x,n+1)/x;
+            }
+        }
+        if(n==0){
+            return 1;
+        }
+        
+        if(n%2==0){
+            double half = myPow(x,n/2);
+            return half*half;
+        }
+        else{
+            return x*myPow(x,n-1);
+        }
+    }
+}
