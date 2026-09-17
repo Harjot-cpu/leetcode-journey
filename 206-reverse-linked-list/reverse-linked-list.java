f9a64c49cf0c484aa6b5ef5d7ae2fb90ce0271bc
Harjot-cpu <sandhuharjot712@gmail.com>
2026-09-17 17:00:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (67.98%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,41 +10,18 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        int l = lengthll(head);
-        int[] arr = new int[l];
-        ListNode temp = head;
-        int count = 0;
-        while(temp!=null){
-            arr[count] = temp.val;
-            count +=1;
-            temp = temp.next;
-        }
-        for(int i = 0;i<l/2;i++){
-            int data = arr[i];
-            arr[i] = arr[l-1-i];
-            arr[l-1-i] = data;
-        }
-        ListNode ans = convert(arr);
-        return ans;
+if(head == null) return null;
+ListNode prv = head;
+    ListNode temp = head;
+    prv = null;
+    while(temp!=null){
 
+        ListNode nxt = temp.next;
+        
+        temp.next = prv;
+        prv = temp;
+        temp = nxt;
     }
-    public int lengthll(ListNode head){
-        int count = 0;
-        ListNode temp = head;
-        while(temp!= null){
-            count +=1;
-            temp = temp.next;
-        }
-        return count;
-    }
-    public ListNode convert(int[] arr){
-        if(arr.length ==0) return null;
-        ListNode hd = new ListNode(arr[0]);
-        ListNode mover = hd;
-        for(int i = 1;i<arr.length;i++){
-            mover.next = new ListNode(arr[i]);
-            mover = mover.next;
-        }
-        return hd;
+    return prv;
     }
 }
